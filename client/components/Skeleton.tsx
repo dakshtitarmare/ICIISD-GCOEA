980cb25172ff0ae08e879c21feb31d395b7c6ded
daksh <dakshtitarmare2006@gmail.com>
2025-12-13 21:28:45 +0530

    mono-clean

--- client/components/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-export default function Skeleton({ height = "80px" }) {
-    return (
-      <div
-        className="animate-pulse bg-gray-300 rounded-xl"
-        style={{ height }}
-      ></div>
-    );
-  }
-  
